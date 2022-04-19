0d54db1ab71c86bf6aab118713ac9e60b909fd5e
apm29 <835170486@qq.com>
2022-04-19 16:13:34 +0800

    file: FadeTransition

---
+++ src/components/transitions/FadeTransition.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import anime from "animejs";
+const onBeforeEnter = function (el: HTMLElement) {
+  console.log("brefore enter");
+  el.style.transition = "opacity 0.5s ease-in-out";
+};
+const onEnter = function (el: HTMLElement, done: Function) {
+  console.log("enter");
+  anime({
+    targets: [el],
+    duration: 500,
+    opacity: [0, 1],
+    complete: () => done(),
+    easing: "easInOutQuad",
+  });
+};
+
+const onAfterEnter = function (el: HTMLElement) {
+  console.log("after enter");
+};
+const onBeforeLeave = function (el: HTMLElement) {
+  console.log("before leave");
+};
+const onLeave = function (el: HTMLElement, done: Function) {
+  console.log("leave");
+  anime({
+    targets: [el],
+    duration: 500,
+    opacity: [1, 0],
+    complete: () => done(),
+  });
+};
+
+const onAfterLeave = function (el: HTMLElement) {
+  console.log("after leave");
+};
+</script>
+<template>
+  <Transition
+    @before-enter="onBeforeEnter"
+    @enter="onEnter"
+    @leave="onLeave"
+    @after-enter="onAfterEnter"
+    @before-leave="onBeforeLeave"
+    @after-leave="onAfterLeave"
+    mode="out-in"
+    v-bind="$attrs"
+  >
+    <slot />
+  </Transition>
+</template>
